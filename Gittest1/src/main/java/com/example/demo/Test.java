3bd3b1420d12edc7a537714cd5b0270e8ca5bce3
ranjan@appensure.com <root@ranjan>
2021-06-29 00:57:34 +0530

    Developer 1 create method

--- Gittest1/src/main/java/com/example/demo/Test.java
+++ Gittest1/src/main/java/com/example/demo/Test.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
 public class Test {
+
+	public Test() {
+		System.out.println("TL Created Test constructor");
+	}
 	
-public Test() {
-	System.out.println("TL Created Test constructor");
-}
+	public void Developer1() {
+		System.out.println("Test.Developer1()");
+		
+	}
 
 }
