3562681fabc66baad78f94439ef362ec2a0c8c9f
ranjan@appensure.com <root@ranjan>
2021-06-29 22:17:48 +0530

    new dev2Test() method create.

--- Gittest1/src/main/java/com/example/demo/Dev2.java
+++ Gittest1/src/main/java/com/example/demo/Dev2.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 public class Dev2 {
 	public Dev2() {
-		System.out.println("Dev2.Dev2()");
+		System.out.println("Dev2.Dev2" + "()");
+
+	}
+	public void dev2Test() {
+		System.out.println("Dev2.dev2Test()");
 		
 	}
 }
